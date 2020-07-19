9644e0aecd427920b93a6cfbe375823ca2fc9a66
Julien <ares11@outlook.fr>
2020-07-20 00:36:37 +0200

    [test] Add more test for CreateAndRunTaskTests
    The new ones allow to test all the cases of the previous tests but with a timezone of + 2h

--- TaskSchedulerTests/CreateAndRunTaskTests.cs
+++ TaskSchedulerTests/CreateAndRunTaskTests.cs
@@ -266,6 +266,268 @@ namespace TaskSchedulerTests
         }
 
 
+        [TestMethod()]
+        public void Should_Start_The_Action_With_A_Correct_Start_And_End_Time_With_UTC_Plus_2()
+        {
+            Task.Run(async () =>
+            {
+                var taskScheduler = TaskSchedulerBuilder.CreateBuilder()
+                .Build();
+
+                SetSchedulerDate(taskScheduler);  // -> control scheduler datetime for avoid end-of-day date issues
+
+                var newTaskId = taskScheduler.TaskAdder
+                   .SetHours(taskScheduler.SchedulerDateTime.AddMilliseconds(500).TimeOfDay, taskScheduler.SchedulerDateTime.AddSeconds(10).TimeOfDay)
+                   .SetAction(async (x) =>
+                   {
+                       while (!x.CancellationToken.IsCancellationRequested) { }
+                   })
+                   .SetTimezone(TimeSpan.Parse("02:00:00"))
+                    .AddTask();
+
+                var getTask = taskScheduler.GetTasksArgWithId(newTaskId);
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                Assert.IsTrue(getTask.Launched);
+            }).GetAwaiter().GetResult();
+        }
+
+        [TestMethod()]
+        public void Should_Start_The_Action_With_A_Correct_Start_And_End_Date_With_UTC_Plus_2()
+        {
+            Task.Run(async () =>
+            {
+                var taskScheduler = TaskSchedulerBuilder.CreateBuilder()
+               .Build();
+
+                SetSchedulerDate(taskScheduler);  // -> control scheduler datetime for avoid end-of-day date issues
+
+                var newTaskId = taskScheduler.TaskAdder
+                    .SetDay(taskScheduler.SchedulerDateTime.AddMilliseconds(500), taskScheduler.SchedulerDateTime.AddSeconds(10))
+                    .SetAction(async (x) =>
+                    {
+                        while (!x.CancellationToken.IsCancellationRequested) { }
+                    })
+                     .SetTimezone(TimeSpan.Parse("02:00:00"))
+                     .AddTask();
+
+                var getTask = taskScheduler.GetTasksArgWithId(newTaskId);
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                Assert.IsTrue(getTask.Launched);
+            }).GetAwaiter().GetResult();
+        }
+
+        [TestMethod()]
+        public void Should_Stop_The_Action_With_A_Correct_Start_And_End_Time_With_UTC_Plus_2()
+        {
+            Task.Run(async () =>
+            {
+                var taskScheduler = TaskSchedulerBuilder.CreateBuilder()
+                .Build();
+
+                SetSchedulerDate(taskScheduler);  // -> control scheduler datetime for avoid end-of-day date issues
+
+                var newTaskId = taskScheduler.TaskAdder
+                   .SetHours(taskScheduler.SchedulerDateTime.AddMilliseconds(200).TimeOfDay, taskScheduler.SchedulerDateTime.AddSeconds(1).TimeOfDay)
+                   .SetAction(async (x) =>
+                   {
+                       while (!x.CancellationToken.IsCancellationRequested) { }
+                   })
+                    .SetTimezone(TimeSpan.Parse("02:00:00"))
+                    .AddTask();
+
+                var getTask = taskScheduler.GetTasksArgWithId(newTaskId);
+                await Task.Delay(TimeSpan.FromSeconds(2));
+                Assert.IsFalse(getTask.Launched);
+                Assert.IsTrue(getTask.CancellationToken.IsCancellationRequested);
+            }).GetAwaiter().GetResult();
+        }
+
+        [TestMethod()]
+        public void Should_Stop_The_Action_With_A_Correct_Start_And_End_Date_With_UTC_Plus_2()
+        {
+            Task.Run(async () =>
+            {
+                var taskScheduler = TaskSchedulerBuilder.CreateBuilder()
+               .Build();
+
+                SetSchedulerDate(taskScheduler);  // -> control scheduler datetime for avoid end-of-day date issues
+
+                var newTaskId = taskScheduler.TaskAdder
+                    .SetDay(taskScheduler.SchedulerDateTime.AddMilliseconds(200), taskScheduler.SchedulerDateTime.AddSeconds(1))
+                    .SetAction(async (x) =>
+                    {
+                        while (!x.CancellationToken.IsCancellationRequested) { }
+                    })
+                     .SetTimezone(TimeSpan.Parse("02:00:00"))
+                     .AddTask();
+
+                var getTask = taskScheduler.GetTasksArgWithId(newTaskId);
+                await Task.Delay(TimeSpan.FromSeconds(2));
+                Assert.IsFalse(getTask.Launched);
+                Assert.IsTrue(getTask.CancellationToken.IsCancellationRequested);
+
+            }).GetAwaiter().GetResult();
+        }
+
+        [TestMethod()]
+        public void Should_Stop_And_Delete_The_Action_With_A_Correct_Start_And_End_Time_With_UTC_Plus_2()
+        {
+            Task.Run(async () =>
+            {
+                var taskScheduler = TaskSchedulerBuilder.CreateBuilder()
+                .Build();
+
+                taskScheduler.Options.DeleteTaskAfterCompleted = true;
+
+                SetSchedulerDate(taskScheduler);  // -> control scheduler datetime for avoid end-of-day date issues
+
+                var newTaskId = taskScheduler.TaskAdder
+                   .SetHours(taskScheduler.SchedulerDateTime.AddMilliseconds(300).TimeOfDay, taskScheduler.SchedulerDateTime.AddSeconds(1).TimeOfDay)
+                   .SetAction(async (x) =>
+                   {
+                       while (!x.CancellationToken.IsCancellationRequested) { }
+                   })
+                    .SetTimezone(TimeSpan.Parse("02:00:00"))
+                    .AddTask();
+
+                var getTask = taskScheduler.GetTasksArgWithId(newTaskId);
+                await Task.Delay(TimeSpan.FromSeconds(2));
+
+                Assert.IsFalse(getTask.Launched);
+                Assert.IsTrue(getTask.CancellationToken.IsCancellationRequested);
+                Assert.IsFalse(taskScheduler.Timers.ContainsKey(newTaskId));
+            }).GetAwaiter().GetResult();
+        }
+
+        [TestMethod()]
+        public void Should_Stop_And_Delete_The_Action_With_A_Correct_Start_And_End_Date_With_UTC_Plus_2()
+        {
+            Task.Run(async () =>
+            {
+                var taskScheduler = TaskSchedulerBuilder.CreateBuilder()
+               .Build();
+
+                SetSchedulerDate(taskScheduler);  // -> control scheduler datetime for avoid end-of-day date issues
+                taskScheduler.Options.DeleteTaskAfterCompleted = true;
+
+                var newTaskId = taskScheduler.TaskAdder
+                    .SetDay(taskScheduler.SchedulerDateTime.AddMilliseconds(200), taskScheduler.SchedulerDateTime.AddSeconds(1))
+                    .SetAction(async (x) =>
+                    {
+                        while (!x.CancellationToken.IsCancellationRequested) { }
+                    })
+                     .SetTimezone(TimeSpan.Parse("02:00:00"))
+                     .AddTask();
+
+                var getTask = taskScheduler.GetTasksArgWithId(newTaskId);
+                await Task.Delay(TimeSpan.FromSeconds(2));
+
+                Assert.IsFalse(getTask.Launched);
+                Assert.IsTrue(getTask.CancellationToken.IsCancellationRequested);
+                Assert.IsFalse(taskScheduler.Timers.ContainsKey(newTaskId));
+            }).GetAwaiter().GetResult();
+        }
+
+
+        [TestMethod()]
+        public void Should_Change_The_Value_Of_Notify_To_True_When_The_Task_Is_Launched_With_The_Subscription_To_Linklaunchedstatus_With_UTC_Plus_2()
+        {
+            Task.Run(async () =>
+            {
+                bool notify = false;
+
+                var taskScheduler = TaskSchedulerBuilder.CreateBuilder()
+               .Build();
+
+                SetSchedulerDate(taskScheduler);  // -> control scheduler datetime for avoid end-of-day date issues
+
+                taskScheduler.Options.DeleteTaskAfterCompleted = true;
+
+                var newTaskId = taskScheduler.TaskAdder
+                    .SetDay(taskScheduler.SchedulerDateTime.AddMilliseconds(500), taskScheduler.SchedulerDateTime.AddSeconds(2))
+                    .SetAction(async (x) =>
+                    {
+                        while (!x.CancellationToken.IsCancellationRequested) { }
+                    })
+                    .LinkLaunchedStatus(async(x) =>
+                    {
+                        notify = x.Launched;
+                    })
+                     .SetTimezone(TimeSpan.Parse("02:00:00"))
+                     .AddTask();
+
+                await Task.Delay(TimeSpan.FromSeconds(1));
+
+                Assert.IsTrue(notify);
+            }).GetAwaiter().GetResult();
+        }
+
+        [TestMethod()]
+        public void Should_Change_The_Value_Of_Notify_To_True_When_The_Task_Is_Stopped_With_The_Subscription_To_Linklaunchedstatus_With_UTC_Plus_2()
+        {
+            Task.Run(async () =>
+            {
+                bool notify = false;
+
+                var taskScheduler = TaskSchedulerBuilder.CreateBuilder()
+               .Build();
+
+                SetSchedulerDate(taskScheduler);  // -> control scheduler datetime for avoid end-of-day date issues
+
+                taskScheduler.Options.DeleteTaskAfterCompleted = true;
+
+                var newTaskId = taskScheduler.TaskAdder
+                    .SetDay(taskScheduler.SchedulerDateTime.AddMilliseconds(200), taskScheduler.SchedulerDateTime.AddSeconds(1))
+                    .SetAction(async (x) =>
+                    {
+                        while (!x.CancellationToken.IsCancellationRequested) { }
+                    })
+                    .LinkLaunchedStatus(async (x) =>
+                    {
+                        notify = x.Launched;
+                    })
+                     .SetTimezone(TimeSpan.Parse("02:00:00"))
+                     .AddTask();
+
+                await Task.Delay(TimeSpan.FromSeconds(2));
+
+                Assert.IsFalse(notify);
+            }).GetAwaiter().GetResult();
+        }
+
+        [TestMethod()]
+        public void Should_Change_The_Value_Of_Notify_To_True_When_The_Task_Is_Finished_With_The_Subscription_To_Linklaunchedstatus_With_UTC_Plus_2()
+        {
+            Task.Run(async () =>
+            {
+                bool notify = false;
+
+                var taskScheduler = TaskSchedulerBuilder.CreateBuilder()
+               .Build();
+
+                SetSchedulerDate(taskScheduler); // -> control scheduler datetime for avoid end-of-day date issues
+
+                taskScheduler.Options.DeleteTaskAfterCompleted = true;
+
+                var newTaskId = taskScheduler.TaskAdder
+                    .SetDay(taskScheduler.SchedulerDateTime.AddMilliseconds(200), taskScheduler.SchedulerDateTime.AddSeconds(1))
+                    .SetAction(async (x) =>
+                    {
+                        await Task.Delay(200);
+                    })
+                    .LinkFinishedStatus(async (x) =>
+                    {
+                        notify = x.Launched;
+                    })
+                     .SetTimezone(TimeSpan.Parse("02:00:00"))
+                     .AddTask();
+
+                await Task.Delay(TimeSpan.FromMilliseconds(600));
+
+                Assert.IsFalse(notify);
+            }).GetAwaiter().GetResult();
+        }
+
         private void SetSchedulerDate(ITaskScheduler taskScheduler)
         {
             taskScheduler.UpdateTaskSchedulerDate(DateTimeOffset.ParseExact("2020-07-18 15:00:00", "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture)); // -> control scheduler datetime for avoid end-of-day date issues
